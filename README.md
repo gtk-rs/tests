864a2fd1904f1c103597a4e3ab4e9e95c66d8ee6
Guillaume Gomez <guillaume1.gomez@gmail.com>
2016-05-05 06:38:27 +0200

    Move resources from examples repo to this one

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# tests
-tests for gtk-rs
+# gtk-rs tests [![Build Status](https://travis-ci.org/gtk-rs/tests.png?branch=master)](https://travis-ci.org/gtk-rs/examples) [![Build status](https://ci.appveyor.com/api/projects/status/glyf8yir9lh8mrh5?svg=true)](https://ci.appveyor.com/project/GuillaumeGomez/tests)
+
+A few gtk-rs tests (and can be considered as examples). To build, just do:
+
+```Shell
+> cargo build
+```
+
+or to enable GTK 3.10 examples as well:
+
+```Shell
+> cargo build --features gtk_3_10
+```
+
+And then run the executables. Please be sure to have installed all the required libraries before building examples (the list is available on [gtk](https://github.com/gtk-rs/gtk/).
+
+## LICENSE
+
+The gtk-rs examples repository is licensed under the MIT license, please refer to the LICENSE and COPYRIGHT files for more information.
---
+++ src/notebook.rs
@@ -0,0 +1,77 @@
+extern crate gtk;
+
+use gtk::prelude::*;
+use gtk::{IconSize, Orientation, ReliefStyle, Widget};
+
+struct Notebook {
+    notebook: gtk::Notebook,
+    tabs: Vec<gtk::Box>
+}
+
+impl Notebook {
+    fn new() -> Notebook {
+        Notebook {
+            notebook: gtk::Notebook::new(),
+            tabs: Vec::new()
+        }
+    }
+
+    fn create_tab(&mut self, title: &str, widget: Widget) -> u32 {
+        let close_image = gtk::Image::new_from_icon_name("window-close",
+                                                         IconSize::Button as i32);
+        let button = gtk::Button::new();
+        let label = gtk::Label::new(Some(title));
+        let tab = gtk::Box::new(Orientation::Horizontal, 0);
+
+        button.set_relief(ReliefStyle::None);
+        button.set_focus_on_click(false);
+        button.add(&close_image);
+
+        tab.pack_start(&label, false, false, 0);
+        tab.pack_start(&button, false, false, 0);
+        tab.show_all();
+
+        let index = self.notebook.append_page(&widget, Some(&tab));
+
+        let notebook_clone = self.notebook.clone();
+        button.connect_clicked(move |_| {
+            let index = notebook_clone.page_num(&widget).unwrap();
+            notebook_clone.remove_page(Some(index));
+        });
+
+        self.tabs.push(tab);
+
+        index
+    }
+}
+
+fn main() {
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+
+    let window = gtk::Window::new(gtk::WindowType::Toplevel);
+
+    window.set_title("Notebook");
+    window.set_position(gtk::WindowPosition::Center);
+    window.set_default_size(640, 480);
+
+    window.connect_delete_event(|_, _| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    let mut notebook = Notebook::new();
+
+    for i in 1..4 {
+        let title = format!("sheet {}", i);
+        let label = gtk::Label::new(Some(&title));
+        notebook.create_tab(&title, label.upcast());
+    }
+
+    window.add(&notebook.notebook);
+    window.show_all();
+    gtk::main();
+}
+
